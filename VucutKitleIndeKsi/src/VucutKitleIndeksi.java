e948377340c9b82e04ad9b3d5f7c283f4b960764
Yunus Emre Gülbahar <yunusemregulbahar1@gmail.com>
2022-10-13 16:09:17 +0300

    Vücüt kitle endeksi hesaplayan program
    Java patikasında yaptığım ödev
    www.patika.dev

---
+++ VucutKitleIndeKsi/src/VucutKitleIndeksi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+
+        double kilo ,boy,indeks;
+
+        System.out.print("Lütfen kilonuzu kg cinsinden giriniz :");
+        kilo= inp.nextDouble();
+        System.out.print("Lütfen kilonuzu metre cinsinden giriniz :");
+        boy= inp.nextDouble();
+
+        indeks=kilo/(boy*boy);
+        System.out.println("Vücut kitle indeksiniz :" +indeks);
+    }
+}
