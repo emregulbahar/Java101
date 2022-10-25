e03355f22e9f5078a98bc6529aeb8d02ca64580c
Yunus Emre Gülbahar <92928519+emregulbahar@users.noreply.github.com>
2022-10-25 13:21:50 +0300

    Update KdvTutar.java

--- KdvTutariHesaplama/src/KdvTutar.java
+++ KdvTutariHesaplama/src/KdvTutar.java
@@ -5,10 +5,14 @@ public class KdvTutar {
     public static void main(String[] args) {
         double tutar,kdvTutar,kdvliTutar;
         Scanner inp =new Scanner(System.in);
+        
         System.out.print("Fiyatı giriniz :");
         tutar= inp.nextDouble();
+        
         boolean yuksekTutar=(tutar>1000);
+        
         boolean dusukTutar=(tutar<=1000);
+        
         double kdvOrani=(yuksekTutar)?0.8:0.18;
         kdvTutar=tutar*kdvOrani;
         System.out.println("Kdv tutarınız  :" +kdvTutar);
